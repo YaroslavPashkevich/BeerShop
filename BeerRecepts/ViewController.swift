3903fc65784f63d618e71ced50a3dba17345685e
Yaroslav.Pashkevich <yaroslav3091989@gmail.com>
2021-11-28 12:38:32 +0300

    Initial Commit

---
+++ BeerRecepts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerRecepts
+//
+//  Created by  Yaroslav on 28.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
